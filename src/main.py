b6bab2484006e0113e752fda8ca155ac76ff8811
Chris <christopher.ogrady@mycit.ie>
2023-11-15 13:50:45 +0000

    2

--- src/main.py
+++ src/main.py
@@ -1,1 +1,57 @@
-print('hi')
+import pandas as pd
+import numpy as np
+
+
+def task1():
+    # labels are 0 - 9
+    # we need 5, 7, 9
+    df = pd.read_csv('fashion-mnist_train.csv')
+    dfa = df[(df['label'] == 5) | (df['label'] == 7) | (df['label'] == 9)]
+    df_label = dfa['label']
+    df_pixels = dfa.loc[:, dfa.columns != 'label']
+
+    # print image of 1, 7, 21
+    
+    return df_label, df_pixels
+
+
+#def task2():
+
+
+
+#def task3():
+
+
+
+#def task4():
+
+
+
+#def task5():
+
+
+
+#def task6():
+
+
+
+#def task7():
+
+
+
+
+
+def main():
+    labels, pixels = task1()
+    print(pixels)
+
+
+    #task2()
+    #task3()
+    #task4()
+    #task5()
+    #task6()
+    #task7()
+
+
+main()
